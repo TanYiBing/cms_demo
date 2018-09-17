9d7cd0c78b2102624d88123da25cbb12445da77d
TanYiBing <820567197@qq.com>
2018-09-17 19:56:54 +0800

    使用ctx.state动态配置html中的url

--- routes/admin.js
+++ routes/admin.js
@@ -4,6 +4,14 @@ const router = require('koa-router')();
 const login = require('./admin/login');
 const user = require('./admin/user');
 
+//配置中间件 获取url地址
+router.use(async (ctx, next) => {
+  // console.log(ctx.host);
+  ctx.state.__HOST__ = `http://${ctx.host}`;
+  await next();
+});
+
+
 router.get('/', (ctx) => {
   ctx.body = 'admin';
 });
