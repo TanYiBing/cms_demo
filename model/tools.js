899ab41baea10f698a0cb094435064eebe8bd07d
TanYiBing <820567197@qq.com>
2018-09-23 21:44:47 +0800

    分类管理模块完成

--- model/tools.js
+++ model/tools.js
@@ -1,9 +1,32 @@
 const md5 = require('md5');
 
 let tools = {
-  md5(str) {
-    return md5(str);
-  }
+	md5(str) {
+		return md5(str);
+	},
+
+	//分级表单数据处理
+	cateToList(data) {
+		//1.获取以及分类
+		let firstArr = [];
+		for (let i = 0; i < data.length; i++) {
+			if (data[i].pid == '0') {
+				firstArr.push(data[i]);
+			}
+		}
+
+		//2.获取二级分类
+		for (let i = 0; i < firstArr.length; i++) {
+			firstArr[i].list = [];
+			//匹配pid和_id
+			for (let j = 0; j < data.length; j++) {
+				if (firstArr[i]._id == data[j].pid) {
+					firstArr[i].list.push(data[j]);
+				}
+			}		
+		}
+		return firstArr;
+	}
 }
 
 module.exports = tools;
--- routes/admin/articlecate.js
+++ routes/admin/articlecate.js
@@ -2,25 +2,54 @@ const router = require('koa-router')();
 const DB = require('../../model/db.js');
 const tools = require('../../model/tools.js')
 
+//分类改造
 router.get('/', async (ctx) => {
-    ctx.body = 'articlecate';
+    let result = await DB.find('articlecate', {});
+    // console.log(result);
+    await ctx.render('admin/articlecate/index', {
+        list: tools.cateToList(result)
+    });
 });
 
 router.get('/add', async (ctx) => {
-    await ctx.render('admin/manager/add.html');
+    let result = await DB.find('articlecate', {'pid': '0'});
+    // console.log(result);
+    await ctx.render('admin/articlecate/add.html', {
+        catelist: result
+    });
 });
 
 router.post('/doAdd', async (ctx) => {
-
-
+    let result = await DB.insert('articlecate', ctx.request.body);
+    ctx.redirect(`${ctx.state.__HOST__}/admin/articlecate`);
 });
 
 router.get('/edit', async (ctx) => {
-
+    let id = ctx.query.id;
+    let result = await DB.find('articlecate', {'_id': DB.getObjectId(id)});
+    let articlecate = await DB.find('articlecate', { 'pid': '0' });
+    // console.log(result);
+    
+    await ctx.render('admin/articlecate/edit.html', {
+        list: result[0],
+        catelist: articlecate
+    });
 });
 
 router.post('/doEdit', async (ctx) => {
-
+    let editData = ctx.request.body;
+    let id = editData.id;
+    let description = editData.description;
+    let keywords = editData.keywords;
+    let pid = editData.pid;
+    let title = editData.title;
+    let updateResult = DB.update('articlecate', { '_id': DB.getObjectId(id)}, {
+        'description': description,
+        'keywords': keywords,
+        'pid': pid,
+        'title': title
+    });
+    ctx.redirect(`${ctx.state.__HOST__}/admin/articlecate`);
 });
 
 router.get('/delete', async (ctx) => {
