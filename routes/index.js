214f6572f555cb529599c66b228cb33c5eeeb8ec
TanYiBing <820567197@qq.com>
2018-10-09 22:28:07 +0800

    前台页面模版更改完成，路径更改完成

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,27 @@
 const router = require('koa-router')();
 
 router.get('/', (ctx) =>{
-  ctx.body = 'index';
+  ctx.render('default/index.html');
+});
+
+router.get('/news', (ctx) => {
+  ctx.render('default/news.html');
+});
+
+router.get('/case', (ctx) => {
+  ctx.render('default/case.html')
+});
+
+router.get('/service', (ctx) => {
+  ctx.render('default/service.html')
+});
+
+router.get('/about', (ctx) => {
+  ctx.render('default/about.html')
+});
+
+router.get('/connect', (ctx) => {
+  ctx.render('default/connect.html')
 });
 
 module.exports = router.routes();
