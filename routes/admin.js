76a8c96b17de8c87b571000d218c9ae5f254aee4
TanYiBing <820567197@qq.com>
2018-10-11 20:21:09 +0800

    把需要重复操作的nav、pathname加到ctx.state上

--- routes/admin.js
+++ routes/admin.js
@@ -15,36 +15,36 @@ const ueditor = require('koa2-ueditor');
 
 //配置中间件 获取url地址
 router.use(async (ctx, next) => {
-  // console.log(ctx.host);
-  ctx.state.__HOST__ = `http://${ctx.host}`;
+    // console.log(ctx.host);
+    ctx.state.__HOST__ = `http://${ctx.host}`;
 
-  let pathname = url.parse(ctx.request.url).pathname.substring(1);
-  // console.log(pathname);
-  let splitUrl = pathname.split('/');
+    let pathname = url.parse(ctx.request.url).pathname.substring(1);
+    // console.log(pathname);
+    let splitUrl = pathname.split('/');
 
-  //配置全局信息
-  ctx.state.G = {
-    userinfo: ctx.session.userinfo,
-    url: splitUrl,
-    prevPage:ctx.request.header['referer'],
-  }
+    //配置全局信息
+    ctx.state.G = {
+        userinfo: ctx.session.userinfo,
+        url: splitUrl,
+        prevPage: ctx.request.header['referer'],
+    }
 
-  //权限判断
-  if(ctx.session.userinfo) {
+    //权限判断
+    if (ctx.session.userinfo) {
 
-	await next();//登陆之后继续乡下匹配路由
+        await next();//登陆之后继续乡下匹配路由
 
-  }else{
-	if((pathname == 'admin/login') || (pathname == 'admin/login/doLogin') || (pathname == 'admin/login/code')){
+    } else {
+        if ((pathname == 'admin/login') || (pathname == 'admin/login/doLogin') || (pathname == 'admin/login/code')) {
 
-	  await next();
-	  
-	}else {
-	  
-	  ctx.redirect('/admin/login');//没有登陆跳转到登录页面
-	  
-	}
-  }
+            await next();
+
+        } else {
+
+            ctx.redirect('/admin/login');//没有登陆跳转到登录页面
+
+        }
+    }
 
 });
 
@@ -62,8 +62,8 @@ router.use('/setting', setting);
 
 //配置富文本编辑器
 router.all('/editor/controller', ueditor(['public', {
-  "imageAllowFiles": [".png", ".jpg", ".jpeg"],
-  "imagePathFormat": "/upload/ueditor/image/{yyyy}{mm}{dd}/{filename}"  // 保存为原文件名
+    "imageAllowFiles": [".png", ".jpg", ".jpeg"],
+    "imagePathFormat": "/upload/ueditor/image/{yyyy}{mm}{dd}/{filename}"  // 保存为原文件名
 }]))
 
 module.exports = router.routes();
--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,52 @@
 const router = require('koa-router')();
+const DB = require('../model/db.js');
+const url = require('url');
 
-router.get('/', (ctx) =>{
-  ctx.render('default/index.html');
+//配置中间件 获取url地址
+router.use(async (ctx, next) => {
+    
+    let navResult = await DB.find('nav', { $or: [{ 'status': '1' }, { 'status': 1 }] }, {}, {
+        sortJson: { 'sort': 1 }
+    });
+    let pathname = url.parse(ctx.request.url).pathname;
+    console.log(pathname);
+
+    ctx.state.nav = navResult;
+    ctx.state.pathname = pathname;
+
+    await next();
 });
 
-router.get('/news', (ctx) => {
-  ctx.render('default/news.html');
+router.get('/', async (ctx) => {
+
+    // 由于状态在后台没写好，可能是字符串或数字，所以使用$or
+    let focusResult = await DB.find('focus', { $or: [{ 'status': '1' }, { 'status': 1 }]}, {}, {
+        sortJson: { 'sort': 1 }
+    });
+    //   console.log(navResult);
+    ctx.render('default/index.html', {
+        focus: focusResult
+    });
 });
 
-router.get('/case', (ctx) => {
-  ctx.render('default/case.html')
+router.get('/news', async (ctx) => {
+    ctx.render('default/news.html');
 });
 
-router.get('/service', (ctx) => {
-  ctx.render('default/service.html')
+router.get('/case', async (ctx) => {
+    ctx.render('default/case.html')
 });
 
-router.get('/about', (ctx) => {
-  ctx.render('default/about.html')
+router.get('/service', async (ctx) => {
+    ctx.render('default/service.html')
 });
 
-router.get('/connect', (ctx) => {
-  ctx.render('default/connect.html')
+router.get('/about', async (ctx) => {
+    ctx.render('default/about.html')
+});
+
+router.get('/connect', async (ctx) => {
+    ctx.render('default/connect.html')
 });
 
 module.exports = router.routes();
